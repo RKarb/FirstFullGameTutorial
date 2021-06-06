81998c1d12e99df0e8f6866695391493fa4be97a
RKarb <36237554+RKarb@users.noreply.github.com>
2021-06-06 14:48:03 +0100

    Score UI
    A Score UI has been added in the top right corner of the player screen, counting each coin which has been picked up by the player

--- Assets/Scripts/CoinCollect.cs
+++ Assets/Scripts/CoinCollect.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinCollect : MonoBehaviour
 {
-
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player")   // Checks if an object with the Player tag has collided with the object carrying this script
         {
-            this.gameObject.SetActive(false);
+            GlobalCoins.coinCount += 1; // Increments the coinCount variable inside the GlobalCoins script by 1
+            this.gameObject.SetActive(false);   // Sets the current object carrying this script to inactive, making it invisible to the player
         }
     }
 }
