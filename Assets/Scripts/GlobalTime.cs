0e8031aa9bdefcfc18e0d991be0f6f19019c8227
RKarb <36237554+RKarb@users.noreply.github.com>
2021-06-06 17:04:24 +0100

    Fade Out and Game Over Text Added
    A fade to black has been added upon the timer hitting 0 as well as white game over text. The fade out and text appear once the timer reaches 0, a coroutine then waits 3 seconds before switching to the empty scene for a respawn/reset

--- Assets/Scripts/GlobalTime.cs
+++ Assets/Scripts/GlobalTime.cs
@@ -10,13 +10,19 @@ public class GlobalTime : MonoBehaviour
     public GameObject timeDisplay;
     public int seconds = 30;
     public bool deductingTime;
+    public GameObject timeUpText;
+    public GameObject fadeOut;
+    public GameObject player;
 
     void Update()
     {
         if (seconds == 0)
         {
+            player.GetComponent<PlayerControls>().enabled = false;
             seconds = 0;
-            SceneManager.LoadScene(1);
+            timeUpText.SetActive(true);
+            fadeOut.SetActive(true);
+            StartCoroutine(RespawningLevel());
         }
         else
         {
@@ -34,5 +40,11 @@ public class GlobalTime : MonoBehaviour
         seconds -= 1;
         timeDisplay.GetComponent<Text>().text = "TIME: " + seconds;
         deductingTime = false;
+    }
+
+    IEnumerator RespawningLevel()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
     }
 }
