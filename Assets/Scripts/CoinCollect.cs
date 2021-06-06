db0d0c7bad249bc0b92bd20338140a5dca0ad0dd
RKarb <36237554+RKarb@users.noreply.github.com>
2021-06-06 15:22:08 +0100

    Audio + Player Model
    BGM and coin pickup FX have been added as well as a base player model: a pink chicken.

--- Assets/Scripts/CoinCollect.cs
+++ Assets/Scripts/CoinCollect.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class CoinCollect : MonoBehaviour
 {
+    public AudioSource collectFX;
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")   // Checks if an object with the Player tag has collided with the object carrying this script
         {
+            collectFX.Play();
             GlobalCoins.coinCount += 1; // Increments the coinCount variable inside the GlobalCoins script by 1
             this.gameObject.SetActive(false);   // Sets the current object carrying this script to inactive, making it invisible to the player
         }
